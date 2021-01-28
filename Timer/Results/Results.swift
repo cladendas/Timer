a7e4b85e4ea5bd93166da09ccea4b2ea010fadf1
cladendas <rvdagi05@yandex.ru>
2021-01-29 01:27:11 +0300

    Эксперименты с отслеживанием результатов

---
+++ Timer/Results/Results.swift
@@ -0,0 +1,14 @@
+//
+//  Results.swift
+//  Timer
+//
+//  Created by cladendas on 29.01.2021.
+//  Copyright © 2021 cladendas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Results: UITableViewController {
+    
+}
---
+++ Timer/Results/ResultsCell.swift
@@ -0,0 +1,14 @@
+//
+//  ResultsCell.swift
+//  Timer
+//
+//  Created by cladendas on 29.01.2021.
+//  Copyright © 2021 cladendas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultsCell: UITableViewCell {
+    
+}
